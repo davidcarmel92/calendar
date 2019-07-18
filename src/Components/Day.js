30c38e8a4cb576890aafdb1a07c42cf427a523f5
David <davidcarmel@davids-mbp.attlocal.net>
2019-07-18 08:53:09 -0700

    initial commit

---
+++ src/Components/Day.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Day() {
+  return (
+    <div className="Day">
+
+    </div>
+  );
+}
+
+export default Day;
---
+++ src/Components/Month.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Day from './Day';
+
+function Month() {
+  return (
+    <div className="Month">
+
+    </div>
+  );
+}
+
+export default Month;
